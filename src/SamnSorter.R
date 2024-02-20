5522f1dd0962f9c8b9c991e325b76a0759d00da9
jimmyliu1326 <jimmyliu1326@yahoo.com>
2024-02-19 17:11:11 -0800

    add phylogenetic placement

--- src/SamnSorter.R
+++ src/SamnSorter.R
@@ -24,9 +24,9 @@ parser$add_argument("-o", "--outdir", type="character", default="./samnsorter",
     help="Directory path to where output files will be written to [%(default)s]")
 parser$add_argument("--tmpdir", type="character", default="",
     help="Directory path to where temporary files will be stored [%(default)s]")
-parser$add_argument("--threshold", type="integer", default=1538,
-    help="Clustering distance threshold for cluster assignments [%(default)s]")
-parser$add_argument("-t", metavar="THREADS", dest="threads", type="double", default=4,
+parser$add_argument("--min_lr", type="double", default=0.25,
+    help="Minimum likelihood weighted ratio for phylogenetic placements [%(default)s]")
+parser$add_argument("-t", metavar="THREADS", dest="threads", type="integer", default=4,
     help="Number of threads to use [%(default)s]")
 parser$add_argument("-v", "--version", action="version", version=paste0("SamnSorter v", version),
     help="Print version information")
@@ -144,23 +144,89 @@ cgmlst_dist_query <- function(
 bh_search <- function(
     tmpdir = NULL # tmp directory path where to run analysis
 ) {
-        cat("Identifying best hit...\n")
-        bh <- apply(dist_out, 1, function(x) { names(which(x == min(x)))[1] })
-        bh_res <<- data.frame("id" = names(bh),
-                             "best_hit" = bh)
-        write.table(bh_res, file.path(tmpdir, "best_hit.tsv"),
-                    sep = "\t", row.names = F, quote = F)
+    cat("Identifying best hit...\n")
+    bh <- apply(dist_out, 1, function(x) { names(which(x == min(x)))[1] })
+    # get cluster ID of the bh
+    clust_path <- Sys.getenv("REF_CLUSTERS")  
+    clust <- fread(clust_path, sep = "\t")
+    bh_clust <- clust$clust[which(clust$id == bh)]
+    bh_res <- data.frame("id" = names(bh),
+                          "best_hit" = bh_clust)
+    write.table(bh_res, file.path(tmpdir, "best_hit.tsv"),
+                sep = "\t", row.names = F, quote = F)
+    return(bh_res)
+}
 
+# phylogenetic placement using APPLES
+pp_search <- function(
+    tmpdir = NULL, # tmp directory path where to run analysis
+    logdir = NULL, # log directory path for storing stdout and stderr
+    outdir = NULL, # output dir
+    threads = 4,   # number of threads
+    min_lr = 0.25  # minimum placement likelihood ratio
+
+) {
+    cat("Running phylogenetic placement...\n")
+    cmd <- paste(
+        "run_apples.py", 
+        "-t", "$REF_NWK", # backbone tree
+        "-d", file.path(outdir, "cgmlst_dist.tsv"), # query distance matrix
+        "-o", file.path(tmpdir, "query.jplace"), # jplace output
+        "-T", threads,
+        ">", file.path(logdir, "APPLES.log"), "2>&1" 
+    )
+    sh(cmd, echo = T)
+    cmd <- paste(
+        "gappa", "examine", "assign",
+        "--jplace-path", file.path(tmpdir, "query.jplace"), # input jplace
+        "--taxon-file", "$REF_TAXONOMY", # input reference taxonomy
+        "--allow-file-overwriting",
+        "--per-query-results",
+        "--out-dir", file.path(tmpdir), # output dir
+        "--threads", threads,
+        ">", file.path(logdir, "gappa.log"), "2>&1" # write log
+    )
+    #sh(cmd, echo = T)
+    # analyze pp results
+    invalid_tax <- c("DISTANT", "enterica", "bongori", "bongori;outgroup")
+    asgmnts <- fread(file.path(tmpdir, "per_query.tsv"), sep = "\t")
+    asgmnts.val <- filter(asgmnts, LWR >= min_lr, !(taxopath %in% invalid_tax))
+    pp_res <- asgmnts.val %>%
+        rename("id" = "name",
+               "pp_clust" = "taxopath") %>%
+        mutate(pp_clust = str_replace_all(pp_clust, ".*;", "")) %>%
+        select(id, pp_clust)
+    write.table(pp_res, file.path(tmpdir, "pp_hit.tsv"),
+                sep = "\t", row.names = F, quote = F)
+    return(pp_res)
 }
 
 # main search 
 search <- function(
     tmpdir = NULL, # tmp directory path where to run analysis
-    outdir = NULL # output dir
+    logdir = NULL, # log directory path for storing stdout and stderr
+    args = NULL    # arg parameters
 ) {
-
         # run best hit
-        bh_search(tmpdir)
+        bh_res <- bh_search(tmpdir)
+        # run pp
+        pp_res <- pp_search(tmpdir, logdir, args$outdir, args$threads, args$min_lr)
+        # merge results
+        search_res <- full_join(pp_res, bh_res, by = "id") %>%
+            mutate(pp_clust = if_else(is.na(pp_clust), "NOVEL", pp_clust))
+        # final prediction by majority voting
+        final_preds <- apply(search_res[,2:ncol(search_res)], 1,
+                             function(x) {
+                                if (length(x) == length(unique(x))) {
+                                    return("NOVEL")
+                                } else {
+                                    return(as.character(x[which.max(table(x))]))
+                                }
+                             })
+        search_res <- cbind(search_res, final_clust = final_preds)
+        cat("Writing cluster assignment results...\n")
+        write.table(search_res, file.path(tmpdir, "samnsorter_res.tsv"),
+                sep = "\t", row.names = F, quote = F)
 }
 
 # main workflow
@@ -172,7 +238,7 @@ tmpdir <- file.path(args$outdir, "6cbdab8f")
 #input_setup(args$file, tmpdir)
 #cgmlst(tmpdir, logdir, args$outdir, args$threads)
 cgmlst_dist_query(tmpdir, logdir, args$outdir)
-search(tmpdir, args$outdir)
+search(tmpdir, logdir, args)
 cat("Workflow completed successfully.\n")
 toc(log = T)
 
